541ea642880c8065ddc6c88f909116253a5e692c
Gyanendra Agnihotri <gyanendra.agnihotri@nagarro.com>
2025-02-16 19:22:05 +0530

    UNIT TEST FIX

--- frontend/todo/src/app/todo/todo.component.spec.ts
+++ frontend/todo/src/app/todo/todo.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { TodoComponent } from './todo.component';
+import { HttpClientTestingModule } from '@angular/common/http/testing'; // Import this
 
 describe('TodoComponent', () => {
   let component: TodoComponent;
@@ -8,7 +9,8 @@ describe('TodoComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ TodoComponent ]
+      declarations: [ TodoComponent ],
+      imports: [HttpClientTestingModule] // Add this to mock HttpClient
     })
     .compileComponents();
 
