1f530e9837b98a30bd430bc9dc775188f8965283
Gyanendra Agnihotri <gyanendra.agnihotri@nagarro.com>
2025-02-18 12:20:27 +0530

    fix port issue

--- backend/restful-web-services/src/main/java/com/springboot/rest/webservices/restfulwebservices/todo/TodoResource.java
+++ backend/restful-web-services/src/main/java/com/springboot/rest/webservices/restfulwebservices/todo/TodoResource.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import org.springframework.http.HttpStatus;
 
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "http://localhost:80")
 @RestController
 public class TodoResource {
 
