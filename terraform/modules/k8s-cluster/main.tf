a8eb7fae4823b98a22c6020a6bf1e6ea9e88b38b
Gyanendra Agnihotri <gyanendra.agnihotri@nagarro.com>
2025-02-27 02:14:07 +0530

    debug

--- terraform/modules/k8s-cluster/main.tf
+++ terraform/modules/k8s-cluster/main.tf
@@ -12,6 +12,12 @@ resource "aws_instance" "bastion" {
   }
 }
 
+resource "local_sensitive_file" "private_key" {
+  content  = var.private_key
+  filename = "${path.module}/ec2-private-key.pem"
+  file_permission = "0600"
+}
+
 resource "aws_instance" "master" {
   ami           = var.ami["master"]
   instance_type = var.instance_type["master"]
@@ -19,16 +25,16 @@ resource "aws_instance" "master" {
   subnet_id     = var.subnet["private"]
   vpc_security_group_ids  = [var.sg["master"]]
   iam_instance_profile = var.iam_instance_profile
-  depends_on = [ aws_instance.bastion ]
+  depends_on = [ aws_instance.bastion, local_sensitive_file.private_key ]
 
   connection {
     type        = "ssh"
     user        = "ubuntu"
-    private_key = file("k8s")
+    private_key = file(local_sensitive_file.private_key.filename)
     host        = self.private_ip
     bastion_host = aws_instance.bastion.public_ip
     bastion_user = "ec2-user"
-    bastion_private_key = file("k8s")
+    bastion_private_key = file(local_sensitive_file.private_key.filename)
   }
 
   provisioner "remote-exec" {
@@ -37,6 +43,11 @@ resource "aws_instance" "master" {
       "sudo sh ./master.sh k8s-master"
     ]
   }
+
+  provisioner "local-exec" {
+    command = "rm -f ${local_sensitive_file.private_key.filename}"
+  }
+
 
   tags = {
     Name = "k8s-master",
