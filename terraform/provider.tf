a42ccebc1c062f395f136586f6e71cddb7f0962c
Gyanendra Agnihotri <gyanendra.agnihotri@nagarro.com>
2025-02-26 16:01:36 +0530

    deps issue

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -4,11 +4,11 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.0"
+      version = "~> 5.88"
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
-      version = "~> 2.0"
+      version = "~> 2.35"
     }
   }
 }
