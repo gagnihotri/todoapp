ff094be64281b8c5ae0d851fafb5a816fff4f944
Gyanendra Agnihotri <gyanendra.agnihotri@nagarro.com>
2025-02-26 17:08:51 +0530

    terraform init upgrade

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,14 +1,12 @@
 terraform {
-  required_version = ">= 1.10.0"
-
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.88"
+      version = "5.88.0"
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
-      version = "~> 2.35"
+      version = "2.35.1"
     }
   }
 }
