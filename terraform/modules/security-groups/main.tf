83dcab2f6e2228dfc57c8e5c4a75d696c7f0b061
Gyanendra Agnihotri <gyanendra.agnihotri@nagarro.com>
2025-02-27 00:07:22 +0530

    syntax

--- terraform/modules/security-groups/main.tf
+++ terraform/modules/security-groups/main.tf
@@ -5,7 +5,7 @@ resource "aws_security_group" "bastion_sg" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks      = ["0.0.0.0/0"]
   }
 
   egress {
@@ -29,7 +29,7 @@ resource "aws_security_group" "master_sg" {
     from_port        = 22
     to_port          = 22
     protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0"]
+    cidr_blocks      = ["0.0.0.0/0"]
   }
 
   ingress {
@@ -77,7 +77,7 @@ resource "aws_security_group" "worker_sg" {
     from_port        = 22
     to_port          = 22
     protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0"]
+    cidr_blocks      = ["0.0.0.0/0"]
   }
 
   ingress {
