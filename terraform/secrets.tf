b659ab55f2565213135b1b9ef1444fc8acece77f
Gyanendra Agnihotri <gyanendra.agnihotri@nagarro.com>
2025-02-27 00:55:55 +0530

    debug

--- terraform/secrets.tf
+++ terraform/secrets.tf
@@ -5,3 +5,7 @@ data "aws_secretsmanager_secret" "private_key" {
 data "aws_secretsmanager_secret_version" "private_key_version" {
   secret_id = data.aws_secretsmanager_secret.private_key.id
 }
+
+output "decoded_secret" {
+  value = jsondecode(data.aws_secretsmanager_secret_version.private_key_version.secret_string)["ec2-key"]
+}
