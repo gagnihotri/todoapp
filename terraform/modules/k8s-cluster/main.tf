c6b5f49295b45467027619fe911cf98384eb2314
Gyanendra Agnihotri <gyanendra.agnihotri@nagarro.com>
2025-02-27 00:20:44 +0530

    syntax

--- terraform/modules/k8s-cluster/main.tf
+++ terraform/modules/k8s-cluster/main.tf
@@ -26,7 +26,7 @@ resource "aws_instance" "master" {
     user        = "ec2-user"
     private_key = var.private_key
     host        = self.private_ip
-    bastion_host = aws_instance..bastion_public_ip
+    bastion_host = aws_instance.bastion_public_ip
     bastion_user = "ec2-user"
     bastion_private_key = var.private_key
   }
