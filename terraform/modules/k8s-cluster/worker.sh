a8b19f055ac408979c530e31579fdb9475ecd925
Gyanendra Agnihotri <gyanendra.agnihotri@nagarro.com>
2025-02-27 18:59:30 +0530

    trigger shell script

--- terraform/modules/k8s-cluster/worker.sh
+++ terraform/modules/k8s-cluster/worker.sh
@@ -86,3 +86,5 @@ echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.
 apt-get update -y
 apt-get install -y kubelet kubeadm kubectl
 apt-mark hold kubelet kubeadm kubectl
+
+kubeadm version
